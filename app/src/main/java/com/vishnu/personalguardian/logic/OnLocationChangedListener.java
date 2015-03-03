f55878efd00c740af78d7aa62e0713bde382c077
Vishnuvathsasarma <vishnuvathsan@gmail.com>
2015-03-03 19:48:17 +0530

    Converted the project into Android Studio Gradle build

---
+++ app/src/main/java/com/vishnu/personalguardian/logic/OnLocationChangedListener.java
@@ -0,0 +1,7 @@
+package com.vishnu.personalguardian.logic;
+
+import android.location.Location;
+
+public interface OnLocationChangedListener {
+	public void onLocationChanged(Location location);
+}
