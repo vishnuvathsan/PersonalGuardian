b182adbd6e6b9f43001a98a5cf2d634f14be22fb
vishnuvathsan <vishnuvathsan@gmail.com>
2016-01-22 00:07:08 +0530

    Build gradle modified UI changes Splash screen modified Notification builder modified

--- app/src/main/java/com/vishnu/personalguardian/logic/TrackingService.java
+++ app/src/main/java/com/vishnu/personalguardian/logic/TrackingService.java
@@ -1,8 +1,5 @@
 package com.vishnu.personalguardian.logic;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.app.IntentService;
 import android.app.Notification;
@@ -19,6 +16,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.os.Vibrator;
+import android.support.v7.app.NotificationCompat;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -28,6 +26,11 @@ import com.vishnu.personalguardian.activity.Main;
 import com.vishnu.personalguardian.activity.PasswordAuthenticator;
 import com.vishnu.personalguardian.activity.Splash;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.vishnu.personalguardian.R.drawable.logo;
+
 /**
  * The TRACKING SERVICE of the application to track and alert
  * 
@@ -36,47 +39,36 @@ import com.vishnu.personalguardian.activity.Splash;
  */
 public class TrackingService extends IntentService {
 
-	private XMLreaderDistance distanceReader;
-	private String currGeocode, destinationGeocode, URL;
-	private boolean isMsgDelivered = false;
 	public static boolean isDestinationReached = false;
 	public static boolean isMsgSent = false;
-	private double currDistance, currTravelledDist, directDistance; // kilometer
-	private double currSpeed; // kilometer per hour
-	private long currTravelledDuration; // hours
-
-	// array list to store the path travelled which can be used to trace back
-	// the path
-	private List<MyLocation> locationTraces;
-
-	private Location location;
-	private LocationReceiver locationReceiver;
-	private int count = 0;
 	public static int deviationCount = 0;
-
-	// The minimum distance to change location Updates in meters
+    public static CountDownTimer smsCancelTimer;
+    // The minimum distance to change location Updates in meters
 	private final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 50;
-
 	// The minimum time between location updates in milliseconds
 	private final long MIN_TIME_BW_UPDATES = 30000;
-
 	// maximum number of occurrence of target deviation
 	private final int MAX_DEVIATION_OCCURANCE = 4;
-
 	// minimum distance in kilometer to consider a deviation in target
 	private final double MIN_DISTANCE_TO_DETECT_DEVIATION = 0.05;
-
 	private final double MIN_DISTANCE_VALUE_FOR_DESTINATION_REACHED = 0.2; // kilometer
 	private final double MIN_TRAVELLING_SPEED = 20.00; // kilometer per hour
 	private final int MIN_COUNT_TO_ACHIEVE_AVERAGE_SPEED = 20;
-
 	// time in milliseconds to cancel a SMS sending
 	private final long MAX_TIME_TO_CANCEL_SMS = 120000;
-
-	private String number;
-
-	public static CountDownTimer smsCancelTimer;
-
+    private XMLreaderDistance distanceReader;
+    private String currGeocode, destinationGeocode, URL;
+    private boolean isMsgDelivered = false;
+    private double currDistance, currTravelledDist, directDistance; // kilometer
+    private double currSpeed; // kilometer per hour
+    private long currTravelledDuration; // hours
+    // array list to store the path travelled which can be used to trace back
+    // the path
+    private List<MyLocation> locationTraces;
+    private Location location;
+    private LocationReceiver locationReceiver;
+    private int count = 0;
+    private String number;
 	private Intent intent;
 
 	private double destLat = 0.00, destLon = 0.00;
@@ -262,8 +254,11 @@ public class TrackingService extends IntentService {
 			boolean lowSpeed, boolean restart) {
 		NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-		Notification notification = new Notification(R.drawable.ic_launcher,
-				"Personal Guardian", System.currentTimeMillis());
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                this);
+
+        Notification notification = new Notification(logo,
+                "Personal Guardian", System.currentTimeMillis());
 		notification.flags = Notification.FLAG_SHOW_LIGHTS
 				| Notification.FLAG_AUTO_CANCEL;
 		notification.ledARGB = Color.MAGENTA;
@@ -271,45 +266,65 @@ public class TrackingService extends IntentService {
 		notification.ledOnMS = 500;
 		notification.tickerText = "State change in personal guardian";
 
-		if (sendSMS && restart) {	
-			//notification asking to restart service and send SMS if not restarted within 2min
+        if (sendSMS && restart) {
+            //notification asking to restart service and send SMS if not restarted within 2min
 			Intent intent2 = new Intent(getApplicationContext(), Splash.class);
 			PendingIntent pendingIntent2 = PendingIntent.getActivity(
 					getApplicationContext(), 0, intent2, 0);
-			notification.setLatestEventInfo(getApplicationContext(),
-					text,
-					"Tap to restart service", pendingIntent2);
-			manager.notify(3, notification);
-			
+//			notification.setLatestEventInfo(getApplicationContext(),
+//					text,
+//					"Tap to restart service", pendingIntent2);
+            notification = builder.setContentIntent(pendingIntent2)
+                    .setSmallIcon(R.drawable.logo).setTicker(text).setWhen(System.currentTimeMillis())
+                    .setAutoCancel(false).setContentTitle("Tap to restart service")
+                    .setContentText(text).build();
+            manager.notify(3, notification);
+
 			//cancel SMS
 			Intent intent3 = new Intent(getApplicationContext(),
 					PasswordAuthenticator.class);
 			PendingIntent pendingIntent3 = PendingIntent.getActivity(
 					getApplicationContext(), 0, intent3, 0);
-			notification.setLatestEventInfo(getApplicationContext(),
-					text,
-					"Tap within " + (MAX_TIME_TO_CANCEL_SMS / 60000) + "min to cancel SMS", pendingIntent3);
-			manager.notify(0, notification);
+//			notification.setLatestEventInfo(getApplicationContext(),
+//					text,
+//					"Tap within " + (MAX_TIME_TO_CANCEL_SMS / 60000) + "min to cancel SMS", pendingIntent3);
+            notification = builder.setContentIntent(pendingIntent3)
+                    .setSmallIcon(R.drawable.logo).setTicker(text).setWhen(System.currentTimeMillis())
+                    .setAutoCancel(false).setContentTitle("Tap within " + (MAX_TIME_TO_CANCEL_SMS / 60000) + "min to cancel SMS")
+                    .setContentText(text).build();
+            manager.notify(0, notification);
 		} else if (sendSMS) {
 			//notification to indicate deviation in target and send SMS in 2min if user didn't tap and cancel it
 			Intent intent = new Intent(getApplicationContext(),
 					PasswordAuthenticator.class);
 			PendingIntent pendingIntent = PendingIntent.getActivity(
 					getApplicationContext(), 0, intent, 0);
-			notification.setLatestEventInfo(getApplicationContext(),
-					"You are deviating from the destination",
-					text, pendingIntent);
-			manager.notify(0, notification);
+//			notification.setLatestEventInfo(getApplicationContext(),
+//					"You are deviating from the destination",
+//					text, pendingIntent);
+            notification = builder.setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.logo).setTicker(text).setWhen(System.currentTimeMillis())
+                    .setAutoCancel(false).setContentTitle("You are deviating from the destination")
+                    .setContentText(text).build();
+            manager.notify(0, notification);
 
 		} else if (lowSpeed) {
-			notification.setLatestEventInfo(getApplicationContext(),
-					"Your travelling speed is lower than the average", text,
-					null);
-			manager.notify(1, notification);
+//			notification.setLatestEventInfo(getApplicationContext(),
+//					"Your travelling speed is lower than the average", text,
+//					null);
+            notification = builder.setContentIntent(null)
+                    .setSmallIcon(R.drawable.logo).setTicker(text).setWhen(System.currentTimeMillis())
+                    .setAutoCancel(false).setContentTitle("Your travelling speed is lower than the average")
+                    .setContentText(text).build();
+            manager.notify(1, notification);
 		} else {
-			notification.setLatestEventInfo(getApplicationContext(),
-					"Personal Guardian status", text, null);
-			manager.notify(2, notification);
+//			notification.setLatestEventInfo(getApplicationContext(),
+//					"Personal Guardian status", text, null);
+            notification = builder.setContentIntent(null)
+                    .setSmallIcon(R.drawable.logo).setTicker(text).setWhen(System.currentTimeMillis())
+                    .setAutoCancel(false).setContentTitle("Personal Guardian status")
+                    .setContentText(text).build();
+            manager.notify(2, notification);
 		}
 	}
 
@@ -348,8 +363,8 @@ public class TrackingService extends IntentService {
 					- locationTraces.get(count - 1).getDistanceToDest();
 			if (differenceToDest > MIN_DISTANCE_TO_DETECT_DEVIATION) {
 				deviationCount++;
-				Log.e("You are deviating from the target", "" + currDistance);
-				Log.e("Deviation Count", "" + deviationCount);
+                Log.e("Deviation from target", "" + currDistance);
+                Log.e("Deviation Count", "" + deviationCount);
 				Toast.makeText(
 						getApplicationContext(),
 						"You are deviating from the target\nDeviation Count: "
